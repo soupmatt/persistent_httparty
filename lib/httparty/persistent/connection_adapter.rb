6264db55e8028b155cc3708fc54a5257bc3c8dab
Matt Campbell <matt@soupmatt.com>
2012-08-30 20:05:41 -0500

    Support a bunch more httparty config parameters

--- lib/httparty/persistent/connection_adapter.rb
+++ lib/httparty/persistent/connection_adapter.rb
@@ -6,6 +6,19 @@ module HTTParty::Persistent
     def connection
       opts = {:url => uri}
       opts.merge!(options[:connection_adapter_options]) if options[:connection_adapter_options]
+      if options[:timeout] && (options[:timeout].is_a?(Integer) || options[:timeout].is_a?(Float))
+        opts.merge!(:read_timeout => options[:timeout], :open_timeout => options[:timeout])
+      end
+      opts.merge!(:debug_output => options[:debug_output]) if options[:debug_output]
+
+      if options[:http_proxyaddr]
+        proxy_opts = {:host => options[:http_proxyaddr]}
+        proxy_opts[:port] = options[:http_proxyport] if options[:http_proxyport]
+        opts[:proxy] = URI::HTTP.build(proxy_opts)
+        opts[:proxy].user = options[:http_proxyuser] if options[:http_proxyuser]
+        opts[:proxy].password = options[:http_proxypass] if options[:http_proxypass]
+      end
+
       PersistentHTTP.new(opts)
     end
   end
--- spec/lib/httparty/persistent/connection_adapter_spec.rb
+++ spec/lib/httparty/persistent/connection_adapter_spec.rb
@@ -17,37 +17,129 @@ describe HTTParty::Persistent::ConnectionAdapter do
       its(:host) { should == uri.host }
       its(:port) { should == uri.port }
 
-      context "setting connection_adapter_options" do
-        let(:connection_adapter_options) { {:name => 'spec_adapter'} }
+      context "when dealing with ssl" do
+        Spec::Matchers.define :use_ssl do
+          match do |connection|
+            connection.use_ssl
+          end
+        end
 
-        its(:name) { should == 'spec_adapter' }
+        context "using port 443 for ssl" do
+          let(:uri) { URI 'https://api.foo.com/v1:443' }
+          it { should use_ssl }
+        end
+
+        context "using port 80" do
+          let(:uri) { URI 'http://foobar.com' }
+          it { should_not use_ssl }
+        end
+
+        context "https scheme with default port" do
+          let(:uri) { URI 'https://foobar.com' }
+          it { should use_ssl }
+        end
+
+        context "https scheme with non-standard port" do
+          let(:uri) { URI 'https://foobar.com:123456' }
+          it { should use_ssl }
+        end
       end
 
-      #context "ssl" do
-        #context "a non-ssl connection is used" do
-          #let(:uri) { URI 'http://www.clear.com/' }
+      context "when setting timeout" do
+        context "to 5 seconds" do
+          let(:options) { {:timeout => 5} }
 
-          #it "should not use ssl" do
-            #subject.use_ssl.should be_false
-          #end
-        #end
+          its(:open_timeout) { should == 5 }
+          its(:read_timeout) { should == 5 }
+        end
 
-        #context "an ssl connection is used" do
-          #let(:uri) { URI 'https://www.secure.com/' }
+        context "and timeout is a string" do
+          let(:options) { {:timeout => "five seconds"} }
 
-          #it "should use ssl" do
-            #subject.use_ssl.should be_true
-          #end
-        #end
+          it "doesn't set the timeout" do
+            subject.open_timeout.should be_nil
+            subject.read_timeout.should be_nil
+          end
+        end
+      end
 
-        #context "an ssl connection with a non-standard port is used" do
-          #let(:uri) { URI 'https://www.secure.com:208934/' }
+      context "when debug_output" do
+        context "is set to $stderr" do
+          let(:options) { {:debug_output => $stderr} }
+          it "has debug output set" do
+            adapter.connection.debug_output.should == $stderr
+          end
+        end
 
-          #it "should use ssl" do
-            #subject.use_ssl.should be_true
-          #end
-        #end
-      #end
+        context "is not provided" do
+          let(:options) { {} }
+          it "does not set_debug_output" do
+            adapter.connection.debug_output.should be_nil
+          end
+        end
+      end
+
+      context 'when providing proxy address and port' do
+        let(:options) { {:http_proxyaddr => '1.2.3.4', :http_proxyport => 8080} }
+
+        its(:proxy_uri) { should == URI.parse('http://1.2.3.4:8080') }
+
+        context 'as well as proxy user and password' do
+          let(:options) do
+            {:http_proxyaddr => '1.2.3.4', :http_proxyport => 8080,
+             :http_proxyuser => 'user', :http_proxypass => 'pass'}
+          end
+          its(:proxy_uri) do
+            uri = URI.parse('http://1.2.3.4:8080')
+            uri.user = 'user'
+            uri.password = 'pass'
+            should == uri
+          end
+        end
+      end
+
+      context "when providing PEM certificates", :pending => true do
+        let(:pem) { :pem_contents }
+        let(:options) { {:pem => pem, :pem_password => "password"} }
+
+        context "when scheme is https" do
+          let(:uri) { URI 'https://google.com' }
+          let(:cert) { mock("OpenSSL::X509::Certificate") }
+          let(:key) { mock("OpenSSL::PKey::RSA") }
+
+          before do
+            OpenSSL::X509::Certificate.should_receive(:new).with(pem).and_return(cert)
+            OpenSSL::PKey::RSA.should_receive(:new).with(pem, "password").and_return(key)
+          end
+
+          it "uses the provided PEM certificate " do
+            subject.cert.should == cert
+            subject.key.should == key
+          end
+
+          it "will verify the certificate" do
+            subject.verify_mode.should == OpenSSL::SSL::VERIFY_PEER
+          end
+        end
+
+        context "when scheme is not https" do
+          let(:uri) { URI 'http://google.com' }
+          let(:http) { Net::HTTP.new(uri) }
+
+          before do
+            Net::HTTP.stub(:new => http)
+            OpenSSL::X509::Certificate.should_not_receive(:new).with(pem)
+            OpenSSL::PKey::RSA.should_not_receive(:new).with(pem, "password")
+            http.should_not_receive(:cert=)
+            http.should_not_receive(:key=)
+          end
+
+          it "has no PEM certificate " do
+            subject.cert.should be_nil
+            subject.key.should be_nil
+          end
+        end
+      end
     end
   end
 end
